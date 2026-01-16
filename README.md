61ce406b943bfe12b79f1f049c88425def958441
Luis-Pozo-Benz <lpozobenz@cifpfbmoll.eu>
2026-01-16 16:12:13 +0100

    Añadido los dtos del login y register al modelo de user

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DWEC_Angular_login
 Hacer un apartado de login para el backend de la prueba de JWT
+
+# Modelos
+Primero hacemos el modelo "user", donde almacenamos los atributos de la clase user del backend. 
+Además de ello, tenemos que concretar los modelos de "login" y "register": para ello, miramos los dtos del backend, que indican qué cosas esperan recibir del frontend (Request) y qué cosas pretenden devolver a este (Response).
--- front-end/users/src/app/models/user.model.ts
+++ front-end/users/src/app/models/user.model.ts
@@ -23,3 +23,33 @@ export interface UserRequest {
 
     email:string;
 }
+
+export interface LoginRequest {
+    username:string;
+
+    password:string;
+}
+
+export interface LoginResponse {
+    token:string;
+    
+    type:string;
+
+    username:string;
+
+    email:string;
+
+    role:string;
+}
+
+export interface RegisterRequest {
+    username:string;
+
+    password:string;
+
+    email:string;
+}
+
+export interface RegisterResponse {
+    message:string;
+}
