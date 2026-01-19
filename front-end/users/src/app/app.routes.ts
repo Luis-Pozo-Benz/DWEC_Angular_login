c8b0cdb73010ee74d3f4ec0ce673c77ca4fcfd32
Luis-Pozo-Benz <lpozobenz@cifpfbmoll.eu>
2026-01-19 20:26:12 +0100

    Updated: routes

--- front-end/users/src/app/app.routes.ts
+++ front-end/users/src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { FormulariLogin } from './components/login-form/login-form';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'formulari-login', pathMatch: 'full'},
-    {path: 'formulari-login', component: FormulariLogin},
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'login', component: FormulariLogin},
 ];
