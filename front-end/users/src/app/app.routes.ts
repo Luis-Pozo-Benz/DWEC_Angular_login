7b424166e98edddc76b5b897fadf8327993a13bc
Luis-Pozo-Benz <lpozobenz@cifpfbmoll.eu>
2026-01-19 15:33:39 +0100

    Empezado el formulario html

--- front-end/users/src/app/app.routes.ts
+++ front-end/users/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { FormulariLogin } from './components/formulari-login/formulari-login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'formulari-login', pathMatch: 'full'},
+    {path: 'formulari-login', component: FormulariLogin},
+];
