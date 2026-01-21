c40544f5209356cb2aca68c8f91c5e0fd9e72008
Luis-Pozo-Benz <lpozobenz@cifpfbmoll.eu>
2026-01-21 18:10:34 +0100

    Update: añadidas las rutas en el app.routes

--- front-end/users/src/app/app.routes.ts
+++ front-end/users/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { FormulariLogin } from './components/login-form/login-form';
+import { UserList } from './components/user-list/user-list';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'login', component: FormulariLogin},
+    {path: 'list', component: UserList},
 ];
